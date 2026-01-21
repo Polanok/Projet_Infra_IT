b95c3f4f352cbc75ce1b4deff052788e4886ad76
Polanok <polanokk@gmail.com>
2026-01-21 10:36:45 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,10 +2,8 @@ DROP TABLE IF EXISTS livres;
 
 CREATE TABLE livres (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     titre TEXT NOT NULL,
     auteur TEXT NOT NULL,
-    annee_publication INTEGER,
-    isbn TEXT,
-    disponible BOOLEAN DEFAULT 1 -- 1 pour disponible, 0 pour emprunté
+    annee_publication INTEGER NOT NULL,
+    date_ajout TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
