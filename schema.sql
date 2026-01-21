3b2e339ab1cdb0e4f843b5f8b9fd226117cfc585
Polanok <polanokk@gmail.com>
2026-01-21 11:13:52 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
+-- -------------------
+-- Table Livres
+-- -------------------
 DROP TABLE IF EXISTS livres;
 
 CREATE TABLE livres (
@@ -8,4 +11,17 @@ CREATE TABLE livres (
     annee_publication INTEGER,
     isbn TEXT,
     disponible BOOLEAN DEFAULT 1 -- 1 pour disponible, 0 pour emprunté
+);
+
+-- -------------------
+-- Table Clients
+-- -------------------
+DROP TABLE IF EXISTS clients;
+
+CREATE TABLE clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    adresse TEXT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
